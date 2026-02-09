3eb7b58c7614a1a75704fd41171aa56ad5bf2471
j-musyoka <mj25802006@gmail.com>
2026-02-10 02:45:52 +0300

    Add Navigation component and Provider component

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import './globals.css'
-
+import Nav from '@/components/Nav';
+import Provider from '@/components/Provider';
 export const metadata = {
   title: 'Promptopia',
   description: 'Discover & Share AI Prompts',
@@ -15,6 +16,8 @@ const RootLayout = ({ children }) => {
 
         <main className='app'>
             {children}
+            <Nav />
+            <Provider />
         </main>
        </body>
     </html>
