b6d288264f958415fe98e2e9ba252100d014221d
j-musyoka <mj25802006@gmail.com>
2026-02-12 01:37:46 +0300

    feat: upgrade NextAuth to v5 with Google OAuth integration]

--- app/api/[...nextauth]/route.js
+++ app/api/[...nextauth]/route.js
@@ -1,20 +1,46 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import Google from "next-auth/providers/google";
+import User from "@models/user";
+import { connectToDB } from "@utils/database";
 
-const handler = NextAuth({
+export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [
-        GoogleProvider({
-            clientId: '',
-            clientSecret: '',
-
+        Google({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
     ],
+    callbacks: {
+        async session({ session }) {
+            if (session?.user?.email) {
+                await connectToDB();
+                const sessionUser = await User.findOne({ email: session.user.email });
+                if (sessionUser) {
+                    session.user.id = sessionUser._id.toString();
+                }
+            }
+            return session;
+        },
+        async signIn({ account, profile }) {
+            try {
+                await connectToDB();
+                const userExists = await User.findOne({ email: profile.email });
 
-    async session ({ session }) {
-
-    },
-
-    async signIn ({ Profile}){
-
-    },
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(" ", "").toLowerCase(),
+                        image: profile.picture,
+                    });
+                }
+                return true;
+            } catch (error) {
+                console.log("Sign in error:", error);
+                return false;
+            }
+        }
+    }
 });
+
+export const GET = handlers.GET;
+export const POST = handlers.POST;
