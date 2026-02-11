2c0696491eda131e5b657960209b7f609965d70b
j-musyoka <mj25802006@gmail.com>
2026-02-12 01:36:52 +0300

    feat: configure Turbopack with path aliases and serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const nextConfig = {
+  serverExternalPackages: ["mongoose"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
+  },
+  turbopack: {
+    resolveAlias: {
+      '@': './',
+      '@models': './model',
+      '@utils': './utils',
+      '@components': './components',
+      '@styles': './styles',
+    },
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      topLevelAwait: true,
+    }
+    return config
+  }
+}
 
 module.exports = nextConfig
